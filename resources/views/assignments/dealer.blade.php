e8da6373e29b52e6ae80dda6a75ed162ec8ba9d3
Steve Pronk <steve@competa.com>
2020-06-22 14:48:26 +0200

    Created some stuff for the dealer assignment :)

--- resources/views/assignments/dealer.blade.php
+++ resources/views/assignments/dealer.blade.php
@@ -2,7 +2,100 @@
 
 @section('content')
     <div id="app" class="row">
-        <div class="col-4">
+        <div class="col-12">
+            <p>
+                <button class="btn btn-secondary" data-toggle="collapse" data-target="[data-collapse=collapseAssignment]" aria-expanded="false">
+                    Assignment
+                </button>
+                <button class="btn btn-secondary" data-toggle="collapse" data-target="[data-collapse=collapseTips]" aria-expanded="false">
+                    Tips & Tricks
+                </button>
+                <button class="btn btn-secondary" data-toggle="collapse" data-target="[data-collapse=collapseResult]" aria-expanded="false">
+                    Required Results
+                </button>
+            </p>
+            <div class="collapse" data-collapse="collapseAssignment">
+                <div class="card card-body mb-3">
+                    <h2 class="d-flex justify-content-between">
+                        Assignment
+                        <button class="fa fa-times btn btn-secondary" data-toggle="collapse" data-target="[data-collapse=collapseAssignment]"></button>
+                    </h2>
+                    <hr/>
+                    <p>
+                        Competa IT gaat nu ook in de auto markt zitten. Hierbij willen wij een applicatie hebben waarbij wij kunnen bijhouden welke
+                        auto's wij op vooraad hebben en hiervan de gegevens. deze gegeven zijn:
+                    </p>
+                    <ul>
+                        <li>Merk</li>
+                        <li>Model</li>
+                        <li>Kleur</li>
+                        <li>Kenteken</li>
+                        <li>Trekhaak</li>
+                        <li>Verbruik</li>
+                        <li>Totale volume tank</li>
+                        <li>Hoveel volume tank</li>
+                        <li>Kilometer tank</li>
+                    </ul>
+                    <p>
+                        Hiervoor is het belangrijk dat als wij een auto verhuren en wij krijgen hem terug, dat we de data kunnen bijwerken en dat
+                        de rest wordt bijgewerkt, denk hieraan van het bijwerken van de kilometer stand waardoor de hoeveel van de tank dan
+                        veranderd, ook moet deze hoeveelheid dan niet in de min komen te staan. Wanneer de tank wordt leeg gereden mag je er van
+                        uit gaan dat de tank helemaal vol wordt gegooit. Deze auto's kunnen ook verkocht worden en dan moet deze ook uit het
+                        systeem kunnen worden gehaald.
+                    </p>
+                </div>
+            </div>
+            <div class="collapse" data-collapse="collapseTips">
+                <div class="card card-body mb-3">
+                    <h2 class="d-flex justify-content-between">
+                        Tips & Tricks
+                        <button class="fa fa-times btn btn-secondary" data-toggle="collapse" data-target="[data-collapse=collapseTips]"></button>
+                    </h2>
+                    <hr/>
+                    <ul>
+                        <li>
+                            Wanneer je de classes opzet voor de Javascript, noem ze naar wat ze zijn. Bijvoorbeeld dat als je voor een auto een
+                            aparte class maakt, dan kan je deze letterlijk "Car" noemen.
+                        </li>
+                        <li>
+                            Gebruik waar nodig zoveel mogelijk de enkelvoud van een woord, bijvoorbeeld "Car" ipv "Cars". Als je een class wilt die
+                            de verschillende auto's regelt dan kan je beter naar een naam kijken zoals "Dealer"
+                        </li>
+                        <li>
+                            Probeer het te houden bij 3 of 4 classes, meer is niet nodig. Gooit ook niet alles in 1 class, dat maakt het weer
+                            onoverzichtelijk.
+                        </li>
+                        <li>
+                            Bedenk van te voren een plan hoe je het in elkaar gaat zitten, begin niet gelijk met coderen.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="collapse" data-collapse="collapseResult">
+                <div class="card card-body mb-3">
+                    <h2 class="d-flex justify-content-between">
+                        Required Results
+                        <button class="fa fa-times btn btn-secondary" data-toggle="collapse" data-target="[data-collapse=collapseResult]"></button>
+                    </h2>
+                    <hr/>
+                    <ul>
+                        <li>Auto's moeten toegevoegd kunnen worden</li>
+                        <li>Auto's moeten gewijzigd kunnen worden</li>
+                        <li>Auto's moeten verwijderd kunnen worden</li>
+                        <li>Wanneer "fuel" gewijzigd wordt, moet de "mileage" automatish gewijzigd worden op basis van "consumption"</li>
+                        <li>Wanneer "mileage" gewijzigd wordt, moet de "fuel" automatish gewijzigd worden op basis van "consumption"</li>
+                        <li>Wanneer "fuel" en "mileage" bijde gewijzigd worden, wordt neits automatisch gewijzigd</li>
+                        <li>Gebruik maken van Javascript classes en deze met elkaar laten communiseren</li>
+                        <li>Code moet op een logishe manier worden opgebouwd</li>
+                        <li>Code moet DRY (Don't repeat yourself) zijn</li>
+                        <li>Redelijk design, moet overzichtelijk en logish zijn</li>
+                        <li>GEEN bootstrap</li>
+                        <li>Plain Javascript</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
             <div class="card">
                 <form id="car-form">
                     <div class="card-header">
