d6643c937c53cbd7371d4bb1e58a481bbbcfedd1
stpronk <stpronk@gmail.com>
2021-01-28 13:29:56 +0100

    Created files for Expenses

---
+++ app/Http/Livewire/Finance/Expenses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Finance;
+
+use Livewire\Component;
+
+class Expenses extends Component
+{
+    public function render()
+    {
+        return view('livewire.finance.expenses');
+    }
+}
---
+++ tests/Unit/Finance/Livewire/ExpenseTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit\Finance\Livewire;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExpenseTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_example_test() {
+        return $this->assertTrue(true);
+    }
+}
