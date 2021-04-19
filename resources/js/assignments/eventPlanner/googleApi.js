ed53e3a50548840176f8f89ab5b597339263e822
stpronk <stpronk@gmail.com>
2021-04-19 17:36:20 +0200

    Removed exposed google API key

--- resources/js/assignments/eventPlanner/googleApi.js
+++ resources/js/assignments/eventPlanner/googleApi.js
@@ -2,7 +2,7 @@ class GoogleApi {
     constructor(app) {
         this.app = app;
         this.wrapper = document.querySelector('.map-wrapper')
-        this.google = require('https://maps.googleapis.com/maps/api/js?key=AIzaSyDiBEuU97vDruJ6ZpMqKxzj-rlEcfSqyT8&callback=initMap&libraries=&v=weekly');
+        this.google = require('');
         this.map = this.init();
     }
 
