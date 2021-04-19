d8376a0fd1bb892524ccb8a280a9582e2d979de0
stpronk <stpronk@gmail.com>
2021-04-19 17:15:10 +0200

    Added a active state the the top menu, should be changed in the future

--- resources/views/layouts/components/navigation/top/general.blade.php
+++ resources/views/layouts/components/navigation/top/general.blade.php
@@ -1,5 +1,5 @@
 <div class="w-100 bg-light shadow">
     @foreach($navigation as $key => $item)
-        <a class="btn btn-light text-dark text-capetilize rounded-0 p-2 mx-2 avenir-bold" href="{{ $item['route'] }}">{{ $item['title'] }} <i class="ml-2 fa fa-{{ $item['icon'] }}"></i></a>
+        <a class="btn btn-light text-dark text-capetilize rounded-0 p-2 mx-2 avenir-bold{{ $item['active'] ? ' top--active' : '' }}" href="{{ $item['route'] }}">{{ $item['title'] }} <i class="ml-2 fa fa-{{ $item['icon'] }}"></i></a>
     @endforeach
 </div>
