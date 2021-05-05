1412bd801620cd2a5f88286e844d5ae3fa511a77
stpronk <stpronk@gmail.com>
2021-05-05 15:11:52 +0200

    Added return type

--- modules/stpronk/view/src/Facades/Navigation.php
+++ modules/stpronk/view/src/Facades/Navigation.php
@@ -15,5 +15,6 @@ use \Illuminate\Support\Facades\Facade;
  */
 class Navigation extends Facade
 {
-    protected static function getFacadeAccessor() { return 'Navigation'; }
+    protected static function getFacadeAccessor(): string
+    { return 'Navigation'; }
 }
