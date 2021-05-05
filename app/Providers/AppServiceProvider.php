cdf099dd221aa04332b9918a85a9808d48fdb4d3
stpronk <stpronk@gmail.com>
2021-05-05 15:35:05 +0200

    Changed the format of all navigation routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,23 +37,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function bootNavigation()
     {
-        Navigation::addItem('Dashboard', 'th', 'home', true, false, null, 0);
+        Navigation::addItem('Dashboard', 'th', 'home', true, false, 'dashboard', 0);
 
-        Navigation::addItem('Assignments','files-o', null, false, false, null, 1)
-            ->addSubItem('Dealer', 'car', 'assignment.dealer', false, false, null, 0)
-            ->addSubItem('Event planner', 'calendar', 'assignment.event-planner', false, false, null, 1);
+        Navigation::addItem('Assignments','files-o', null, false, false, 'assignments', 1)
+            ->addSubItem('Dealer', 'car', 'assignment.dealer', false, false, 'assignments', 0)
+            ->addSubItem('Event planner', 'calendar', 'assignment.event-planner', false, false, 'assignments', 1);
 
-        Navigation::addItem('Finance', 'money', 'finance.index', true, false, null, 2);
+        Navigation::addItem('Finance', 'money', 'finance.index', true, false, 'finance', 2);
 
         /**
          * ---------- ADMIN ROUTES ----------
          */
-        Navigation::addItem('Users', 'users', 'users.index', true, true, null, 3);
+        Navigation::addItem('Users', 'users', 'users.index', true, true, 'users', 3);
 
-        Navigation::addItem('Site', 'globe', 'site.index', true, true, null, 4, ['hide-sub-menu' => true])
-            ->addSubItem('', 'home', 'site.index', true, true, null, 0)
-            ->addSubItem('Analytics', 'search', 'site.analytics', true, true, null, 1)
-            ->addSubItem('Portfolio', 'briefcase', 'site.portfolio', true, true, null, 2);
+        Navigation::addItem('Site', 'globe', 'site.index', true, true, 'site', 4, ['hide-sub-menu' => true])
+            ->addSubItem('', 'home', 'site.index', true, true, 'site', 0)
+            ->addSubItem('Analytics', 'search', 'site.analytics', true, true, 'site', 1)
+            ->addSubItem('Portfolio', 'briefcase', 'site.portfolio', true, true, 'site', 2);
 
     }
 }
