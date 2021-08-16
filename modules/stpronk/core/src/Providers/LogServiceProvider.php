bf99141bccb532a03221620dd6b3f9954874132e
StPronk <stpronk@gmail.com>
2021-08-16 16:38:47 +0200

    =Added loader files to make the logging section

---
+++ modules/stpronk/core/src/Providers/LogServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Stpronk\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+	//
+    }
+x
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
